7f23d15d8b7d0331c338cf95422026f33c9e02a2
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2018-09-21 20:45:52 -0500

    made initial files and added .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
