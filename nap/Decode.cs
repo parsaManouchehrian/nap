1b53529129b28029679756831becfc61223195f9
Glen Vollmer <glenvollmer@Glens-MacBook-Pro.local>
2018-10-09 19:25:12 -0500

    cleaned up decode file and added aditional methods

--- nap/Decode.cs
+++ nap/Decode.cs
@@ -4,17 +4,19 @@ namespace nap
 {
     public class Decode
     {
-        public static void Decode(string filePath, string ext)
+        public static void CheckExt(string filePath, string ext)
         {
         	switch(ext)
         	{
         		// add more case statements for other file types when the time comes
-        		
+
         		case ".mp3":
         			Mp3Decode(filePath);
+        			break;
 
         		case ".wav":
         			WavDecode(filePath);
+        			break;
 
         		default:
         			// add a conditional to check and see if the appropriate flag was passed to decode a non supported file
@@ -25,12 +27,19 @@ namespace nap
 
         private static void Mp3Decode(string filePath)
         {
+        	// add mp3 decode logic from reference from external libraries :)
         	Console.WriteLine(filePath);
         }
 
         private static void WavDecode(string filePath)
         {
         	Console.WriteLine(filePath);
+        }
+
+        public static void StopPlayback()
+        {
+        	// add stop logic from reference from external libraries :)
+        	Console.WriteLine("playback is stopped");
         }
     }
 }
